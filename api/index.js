eb1ad746b5e443cdc0a1159e3a4422ed362a6567
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2022-04-18 19:05:57 +0530

    deployed api, cors policy

--- api/index.js
+++ api/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import post from './routes/post.js';
 import user from './routes/user.js';
+import cors from 'cors';
 
 const app= express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use('/user',user);
 app.use('/post',post);
 
 const PORT = process.env.PORT || 3001;
+
+app.get('/',(req,res)=>{
+  res.send("Insta Clone API");
+});
 
 app.listen(PORT,()=>{
   console.log("Server is started");
